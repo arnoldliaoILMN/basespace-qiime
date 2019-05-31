aa1345e22cbd6c6cf4d294aebf694b8fcf71996b
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2019-05-31 15:38:51 -0700

    DEV: Update upstream to use QIIME2 API
    All steps have been put together here

--- scripts/upstream.py
+++ scripts/upstream.py
@@ -2,79 +2,151 @@
 
 import sys
 import json
-import logging
+import numpy as np
 
-from glob import glob
+import qiime2 as q2
+import qiime2.plugins.demux.actions as q2_demux
+import qiime2.plugins.feature_table.actions as q2_feature_table
+import qiime2.plugins.quality_filter.actions as q2_quality_filter
+import qiime2.plugins.deblur.actions as q2_deblur
+import qiime2.plugins.dada.actions as q2_dada2
+import qiime2.plugins.phylogeny.actions as q2_phylogeny
+import qiime2.plugins.feature_classifier.actions as q2_feature_classifier
+
+from math import floor
 from os.path import join
-
-from qcli import qcli_system_call
-from biom import load_table
-from skbio.util import create_dir
-
-
-def system_call(cmd, shell=True):
-    """Simple wrapper around qcli_system_call"""
-    info = qcli_system_call(cmd)
-
-    if info[2] != 0:
-        logging.error('STDOUT: '+info[0])
-        logging.error('STDERR: '+info[1])
-        return sys.exit(info[2])
+from os import makedirs
+from collections import namedtuple
 
 
 def main():
-
     with open('/data/input/AppSession.json', 'U') as fd_json:
         app = json.load(fd_json)
 
-    jobs = '1'
-    # get command attributes, etc
+    # this should be a parameter
+    deblur = True
+    samples = []
+
+    # TODO: confirm the size of the AMI is fixed to 12 cores, if this
+    # is the case then lets always split over 11 jobs
+    jobs = 11
+
+    # handy for storing sample information
+    Sample = namedtuple('Sample', 'id name path length')
+
+    # TODO: Making a parser of this whole thing would be better (could also
+    # include some unit testing). Also encapsulating everything in one object
+    # would be better
     for item in app['Properties']['Items']:
         if item['Name'] == 'Input.Projects':
             project_id = item['Items'][0]['Id']
         if item['Name'] == 'Input.number-of-jobs':
             jobs = item['Content']
 
+        if item['Name'] == 'Input.sample-id':
+            for sample in item['Items']:
+                s = Sample(sample['Id'], sample['Name'])
+                s.length = sample['Read1']
+
+                # path to the samples
+                path = '/data/input/samples/' + s.id
+                # TODO: how does this path look like?
+                s.path = join(path, 'xxxx.R1.fastq.gz')
+
+                samples.append(s)
+
     # from BaseSpace's documentation
-    input_dir = '/data/input/samples/'
+    workspace = '/data/scratch/'
+
     base = join('/data/output/appresults/', project_id)
-    output_dir = join(base, 'sl-out')
+    output_dir = join(base, 'upstream-results')
 
     # for sanity
-    create_dir(output_dir)
+    makedirs(output_dir, exist_ok=True)
 
-    # split libraries
-    cmd = ("multiple_split_libraries_fastq.py "
-           "-i '{input_dir}' -o '{output_dir}'")
-    params = {'input_dir': input_dir, 'output_dir': output_dir}
+    # this file needs to be created based on the sequence files found in the
+    # run folder, note that this manifest would only be good for BaseSpace
+    # therefore we don't save this in the output folder
+    manifest_fp = join(workspace, 'manifest.tsv')
+    with open(manifest_fp, 'w') as f:
+        f.write('sample-id\tabsolute-filepath\n')
+        for sample in samples:
+            f.write('{}\t{}\n'.format(sample.id, sample.path))
 
-    system_call(cmd.format(**params))
-    for log_file in glob(join(output_dir, 'log_*')):
-        with open(log_file, 'U') as fd_log:
-            print fd_log.read()
+    # import the sequence data based on the manifest-provided information
+    demux = q2.Artifact.import_data('SampleData[SequencesWithQuality]',
+                                    manifest_fp,
+                                    'SingleEndFastqManifestPhred33')
 
-    # OTU picking
-    input_dir = join(output_dir, 'seqs.fna')
-    output_dir = join(base, 'closed-ref')
-    cmd = ("pick_closed_reference_otus.py "
-           "-i '{input_seqs}' -o '{output_dir}'")
-    params = {'input_seqs': input_dir, 'output_dir': output_dir, 'jobs': jobs}
+    # based on a conversation with Arnold we determined that a resonable
+    # default for sequence trimming would be 70% of the sequence lengths
+    trim_length = floor(np.array([s.length for s in samples]).mean() * 0.70)
 
-    # see https://github.com/biocore/qiime/issues/2034
-    if jobs != '1':
-        cmd += ' -a -O {jobs}'
+    # create a summary visualization of the quality scores
+    summary, = q2_demux.summarize(demux)
+    summary.save(join(output_dir, 'sequences.qzv'))
 
-    system_call(cmd.format(**params))
-    for log_file in glob(join(output_dir, 'log_*')):
-        with open(log_file, 'U') as fd_log:
-            print fd_log.read()
+    # quality controlling the sequences, the defaults should be good enough
+    filtered, _ = q2_quality_filter(demux)
+    filtered.save(join(output_dir, 'quality-controlled.sequences.qza'))
 
-    input_dir = join(output_dir, 'otu_table.biom')
-    output_dir = join(base, 'closed-ref', 'table-summary.txt')
-    cmd = ("biom summarize-table "
-           "-i '{input_table}' -o '{output_summary}'")
-    params = {'input_table': input_dir, 'output_summary': output_dir}
-    system_call(cmd.format(**params))
+    # https://developer.basespace.illumina.com/docs/content/documentation/
+    # apptools/formbuilder-overview#AdvancedToggling
+    # TODO: In the form create a series of parameters that can be modified
+    # depending on what method is selected
+    if deblur:
+        feature_table, representative_sequences, _ = \
+            q2_deblur.denoise_16S(demultiplexed_seqs=filtered,
+                                  sample_stats=True, trim_length=trim_length,
+                                  jobs_to_start=jobs, hashed_feature_ids=True)
+    else:
+        feature_table, representative_sequences, _ = \
+            q2_dada2.denoise_single(demultiplexed_seqs=filtered,
+                                    trunc_len=trim_length, n_threads=jobs,
+                                    hashed_feature_ids=True)
+
+    # save both outputs
+    feature_table.save(join(output_dir, 'feature-table.qza'))
+    representative_sequences.save(join(output_dir,
+                                       'representative-sequences.qza'))
+
+    # create a summary of the feature table
+    summary, = q2_feature_table.summarize(table=feature_table)
+    summary.save('feature-table-summary.qzv')
+
+    # build a *quick* phylogenetic tree using MAFFT. We should build this tree
+    # using SEPP, however that would take more resources than what this AMI has
+    _, _, _, rooted_tree = q2_phylogeny.align_to_tree_mafft_fasttree(
+        sequences=representative_sequences,
+        n_threads=jobs)
+    rooted_tree.save(join(output_dir, 'rooted-tree.qza'))
+
+    # taxonomic classification
+    # TODO: in the form list the four options for the classifiers
+    classifier = q2.Artifact.load(join(workspace,
+                                       'gg-13-8-99-515-806-nb-classifier.qza'))
+    classification, = q2_feature_classifier.classify_sklearn(
+            reads=representative_sequences, classifier=classifier,
+            reads_per_batch=1000, n_jobs=jobs)
+    classification.save(join(output_dir, 'taxonomy.qza'))
+
+    # Generated outputs are:
+    #
+    # For record keeping and in incase the users would like to process their
+    # sequences in a different way:
+    # * demultiplexed sequences
+    # * sequence quality summary
+    # * quality controlled sequences
+    #
+    # For the qiime downstream app:
+    # * feature table (feature-table.qza)
+    # * feature table summary per sample & feature (feature-table-summary.qza)
+    # * representative sequences (representative-sequences.qza)
+    # * rooted tree (rooted-tree.qza)
+    # * taxonomic classifications (taxonomy.qza)
+    #
+    # Ask around to see if it would make sense to save as TSV, newick and FASTA
+    # files
 
     return 0
 
